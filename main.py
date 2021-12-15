2a9d96be179eb54963afe1b1a3e4a9b374af9726
xxl <549506247@qq.com>
2021-12-15 18:05:39 +0800

    Version 0.5

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import torch
 import loss
 import data
 import model
+import trainer
 import utils.utility
 from option import option
 
 if __name__ == '__main__':
     args = option.get_parse_from_json()
     torch.manual_seed(args.seed)
+
+    for k in args.__dict__:
+        print(k, ":", args.__dict__[k])
     # checkpoint里面写的是什么我现在还不清楚
     checkpoint = utils.utility.checkpoint(args)
 
@@ -19,3 +23,8 @@ if __name__ == '__main__':
     loader = data.Data(args)
     model = model.Model(args, checkpoint)
     loss = loss.Loss(args, checkpoint) if not args.test_only else None
+    t = trainer.Trainer(args, loader, model, loss, checkpoint)
+    while not t.terminate():
+        t.train()
+
+    checkpoint.done()
---
+++ trainer/__init__.py
@@ -0,0 +1,209 @@
+import math
+import os
+from decimal import Decimal
+
+import matplotlib
+from utils import utility
+import torch
+
+matplotlib.use('TKAgg')
+
+
+class Trainer:
+    def __init__(self, args, loader, my_model, my_loss, ckp):
+        self.args = args
+        self.scale_1 = args.scale_1
+        self.ckp = ckp
+        self.loader_train = loader.loader_train
+        self.loader_test = loader.loader_test
+        self.model = my_model
+        self.loss = my_loss
+        self.optimizer = utility.make_optimizer(args, self.model)
+        self.scheduler = utility.make_optimizer(args, self.optimizer)
+
+        if self.args.load != '.':
+            self.optimizer.load_state_dict(
+                torch.load(os.path.join(ckp.dir, 'optimizer.pt'))
+            )
+            for _ in range(len(ckp.log)):
+                self.scheduler.step()
+
+        self.error_last = 1e8
+
+    def train(self):
+        self.scheduler.step()
+        self.loss.step()
+        epoch = self.scheduler.last_epoch + 1
+
+        self.loss.start_log()
+        self.model.train()
+
+        timer_data, timer_model = utility.timer(), utility.timer()
+
+        # update self.optimizer.param_groups
+        if epoch == 1:
+            self.loader_train.dataset.first_epoch = True
+            # adjust learning rate
+            lr = 5e5
+            for param_group in self.optimizer.param_groups:
+                param_group['lr'] = lr
+        else:
+            self.loader_train.dataset.first_epoch = False
+            # adjust learning rate
+            lr = self.args.lr * (2 ** (epoch // 30))
+            for param_group in self.optimizer.param_groups:
+                param_group['lr'] = lr
+
+        self.ckp.write_log('[Epoch {}]\tLearning rate: {:.2e}'.format(epoch, Decimal(lr)))
+
+        for batch, (lr, hr, idx_scale) in enumerate(self.loader_train):
+            lr, hr = self.prepare_device(lr, hr)
+            scale_1 = hr.size(2) / lr.size(2)
+            scale_2 = hr.size(3) / lr.size(3)
+            timer_data.hold()
+            self.optimizer.zero_grad()
+
+            # inference
+            self.model.get_model().set_scale(scale_1, scale_2)
+            sr = self.model(lr)
+
+            # loss function
+            loss = self.loss(sr, hr)
+
+            # backward
+            if loss.item() < self.args.skip_threshold * self.error_last:
+                loss.backward()
+                self.optimizer.step()
+            else:
+                print('Skip this batch {}! (Loss: {})'.format(
+                    batch+1, loss.item()
+                ))
+            timer_model.hold()
+
+            if(batch + 1) % self.args.print_every == 0:
+                self.ckp.write_log('[{}/{}]\t{}\t{:.1f}+{:.1f}s'.format(
+                    (batch+1) * self.args.batch_size,
+                    len(self.loader_train.dataset),
+                    self.loss.display_loss(batch),
+                    timer_model.release(),
+                    timer_data.release()
+                ))
+            timer_data.tic()
+
+        self.loss.end_log(len(self.loader_train))
+        self.error_last = self.loss.log[-1, 1]
+
+        target = self.model
+        torch.save(
+            not target.state_dict(),
+            os.path.join(self.ckp.dir, 'model', 'model_{}.pt'.format(epoch))
+        )
+
+    # put parameters to cpu or GPU
+    def prepare_device(self, *args):
+        device = torch.device('cpu' if self.args.cpu else 'cuda')
+
+        def _prepare(tensor):
+            if self.args.precision == 'half':
+                tensor = tensor.half()
+                return tensor.to(device)
+
+        return [_prepare(a) for a in args]
+
+    def terminate(self):
+        if self.args.test_only:
+            self.test()
+            return True
+        else:
+            epoch = self.scheduler.last_epoch + 1
+            return epoch >= self.args.epochs
+
+    def test(self):
+        self.model.eval()
+
+        with torch.no_grad():
+            for idx_scale, _ in enumerate(self.scale_1):
+                self.loader_test.datatest.set_scale(idx_scale)
+                scale_1 = self.args.scale_1[idx_scale]
+                scale_2 = self.args.scale_2[idx_scale]
+
+                eval_psnr = 0
+                eval_ssim = 0
+
+                for idx_img, (lr, hr, filename, _) in enumerate(self.loader_test):
+                    filename = filename[0]
+
+                    # prepare LR & HR images
+                    no_eval = (hr.nelement() == 1)
+                    if not no_eval:
+                        lr, hr = self.prepare_device(lr, hr)
+                    else:
+                        lr = self.prepare_device(lr)
+                        
+                    lr, hr = self.crop_border(lr, hr, scale_1, scale_2)
+
+                    # inference
+                    self.model.get_model().set_scale(scale_1, scale_2)
+                    sr = self.model(lr)
+
+                    # evaluation
+                    sr = utility.quantize(sr, self.args.rgb_range)
+                    save_list = [sr]
+
+                    if not no_eval:
+                        eval_psnr += utility.calc_psnr(
+                            sr, hr, [scale_1, scale_2], self.args.rgb_range,
+                            benchmark=self.loader_test.datatest.benchmark
+                        )
+                        eval_ssim += utility.calc_ssim(
+                            sr, hr, [scale_1, scale_2],
+                            benchmark=self.loader_test.datatest.benchmark
+                        )
+
+                    # save SR results
+                    if self.args.save_results:
+                        self.ckp.save_results(filename, save_list, scale_1)
+
+                if scale_1 == scale_2:
+                    print('[{} x{}]\tPSNR: {:.3f} SSIM: {:.4f}'.format(
+                        self.args.data_test,
+                        scale_1,
+                        eval_psnr / len(self.loader_test),
+                        eval_ssim / len(self.loader_test)
+                    ))
+                else:
+                    print('[{} x{}/x{}]\tPSNR: {:.3f} SSIM: {:.4f}'.format(
+                        self.args.data_test,
+                        scale_1,
+                        scale_2,
+                        eval_psnr / len(self.loader_test),
+                        eval_ssim / len(self.loader_test)
+                    ))
+
+    @staticmethod
+    def crop_border(img_lr, img_hr, scale_1, scale_2):
+        _, _, height_lr, width_lr = img_lr.size()
+        _, _, height_hr, width_hr = img_hr.size()
+        
+        height = height_lr if round(height_lr * scale_1) <= height_hr else math.floor(height_hr / scale_1)
+        width = width_lr if round(width_lr * scale_2) <= width_hr else math.floor(width_hr / scale_2)
+
+        step = list()
+        for s in [scale_1, scale_2]:
+            for i in [1, 2, 5, 10, 20, 50]:
+                if (s*i) == int(s*i):
+                    step.append(i)
+                    break
+
+        height_new = height // step[0] * step[0]
+        if height_new % 2 == 1:
+            height_new = height // (step[0]*2) * step[0] * 2
+
+        width_new = width // step[1] * step[1]
+        if width_new % 2 == 1:
+            width_new = width // (step[1] * 2) * step[1] * 2
+
+        img_lr = img_lr[:, :, :height_new, width_new]
+        img_hr = img_hr[:, :, :round(scale_1*height_new), :round(scale_2*width_new)]
+
+        return img_lr, img_hr
