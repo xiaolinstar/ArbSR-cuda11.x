1cd7a98704abb1f336f861785c0f2f3bdd1714f7
xxl <549506247@qq.com>
2021-12-19 17:45:44 +0800

    重构了train test 入口为main args.test_only 决定训练or测试

---
+++ main.py
@@ -0,0 +1,33 @@
+import torch
+import data
+import loss
+import model
+import trainer
+import utils
+from option import option
+
+
+def get_trainer(cp, params):
+    torch.manual_seed(params.seed)
+
+    # use PyTorch to train, what are needed:
+    # my_loader
+    # my_model
+    # optimizer
+    my_loader = data.Data(params)
+    my_model = model.Model(params, cp)
+    my_loss = loss.Loss(params, cp)
+    return trainer.Trainer(params, my_loader, my_model, my_loss, cp)
+
+
+if __name__ == '__main__':
+    args = option.get_parse_from_json()
+    checkpoint = utils.utility.checkpoint(args)
+    t = get_trainer(checkpoint, args)
+
+    while not t.terminate():
+        if args.test_only:
+            t.test()
+        else:
+            t.train()
+    checkpoint.done()
